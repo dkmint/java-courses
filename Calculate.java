9bb518eba71f2cd8076d2252f25a47d196cff46a
scor3141o <dkosimov@gmail.com>
2019-12-03 22:12:54 +0100

    user input

--- Calculate.java
+++ Calculate.java
@@ -1,18 +1,31 @@
+// Version 1.
 public class Calculate {
     public static void main(String[] arg) {
         System.out.println("Calculate...");
-        int first = Integer.valueOf(arg[0]);
-        int second = Integer.valueOf(arg[1]);
-        int sum = first + second;
-        int minus = first - second;
-        int mal = first * second;
-        float div = first / (float)second;
-        int xxor = first ^ second;
-        System.out.println("Sum : " + sum);
-        System.out.println(String.format("%s + %s = %s", first, second, sum));
-        System.out.println(String.format("%s - %s = %s", first, second, minus));
-        System.out.println(String.format("%s * %s = %s", first, second, mal));
-        System.out.println(String.format("%s / %s = %s", first, second, div));
-        System.out.println(String.format("%s ^ %s = %s", first, second, xxor));
+        double first = Double.valueOf(arg[0]);
+        double second = Double.valueOf(arg[1]);
+        System.out.println(String.format("%s + %s = %s", first, second, add(first, second)));
+        System.out.println(String.format("%s - %s = %s", first, second, subtract(first, second)));
+        System.out.println(String.format("%s * %s = %s", first, second, multiple(first, second)));
+        System.out.println(String.format("%s / %s = %s", first, second, div(first, second)));
+        System.out.println(String.format("%s ^ %s = %s", first, second, expand(first, (int)second)));
+    }
+    public static double add(double first, double second) {
+        return first + second;
+    }
+    public static double subtract(double first, double second) {
+        return first - second;
+    }
+    public static double multiple(double first, double second) {
+        return first * second;
+    }
+    public static double div(double first, double second) {
+        return first / second;
+    }
+    public static double expand(double first, double second) {
+        return  (int)first ^ (int)second;
+//        return b;
     }
 }
+
+
